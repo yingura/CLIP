f91be76f726f95d20da10ecf0a1f1b90561602c9
Gina <gina_brasen@cox.net>
2024-10-03 16:55:30 -0700

    Update model.py to EmScore fit

--- clip/model.py
+++ clip/model.py
@@ -340,19 +340,22 @@ class CLIP(nn.Module):
     def encode_image(self, image):
         return self.visual(image.type(self.dtype))
 
-    def encode_text(self, text):
+    def encode_text(self, text, local=False):
         x = self.token_embedding(text).type(self.dtype)  # [batch_size, n_ctx, d_model]
-
+    
         x = x + self.positional_embedding.type(self.dtype)
         x = x.permute(1, 0, 2)  # NLD -> LND
         x = self.transformer(x)
         x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.ln_final(x).type(self.dtype)
-
-        # x.shape = [batch_size, n_ctx, transformer.width]
-        # take features from the eot embedding (eot_token is the highest number in each sequence)
-        x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection
-
+    
+        if local:
+            x = x @ self.text_projection
+        else:
+            # x.shape = [batch_size, n_ctx, transformer.width]
+            # take features from the eot embedding (eot_token is the highest number in each sequence)
+            x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection
+      
         return x
 
     def forward(self, image, text):
